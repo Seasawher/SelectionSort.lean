0e9a0b0d9d3acf8f47dacaf39249df94ab699b8d
Seasawher <tellagoodstory@icloud.com>
2024-05-12 21:20:38 +0900

    ビルドが通るようにする

--- SelectionSort/Basic.lean
+++ SelectionSort/Basic.lean
@@ -1,1 +1,14 @@
-def hello := "world"
+/-
+# 選択ソート
+選択ソートは，次のような手順で行われるソートアルゴリズムです．
+
+-/
+variable {α : Type} [Ord α]
+
+/-- 配列 `arr` が与えられたとき, `arr[i]` から `arr` の最後尾までの部分のうち
+最小の要素 `arr[min]` を探し，それを `arr[i]` と交換する -/
+def selectionSortLoop (arr : Array α) (i : Fin arr.size) : Array α :=
+  if i ≥ arr.size then
+    arr
+  else
+    sorry
--- lakefile.lean
+++ lakefile.lean
@@ -4,9 +4,6 @@ open Lake DSL
 package «SelectionSort» where
   -- add package configuration options here
 
+@[default_target]
 lean_lib «SelectionSort» where
   -- add library configuration options here
-
-@[default_target]
-lean_exe «selectionsort» where
-  root := `Main
