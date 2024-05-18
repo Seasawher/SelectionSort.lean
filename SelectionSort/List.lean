11e7b44339200e06e9f891730a97f7d45e77adb1
Seasawher <tellagoodstory@icloud.com>
2024-05-19 03:37:27 +0900

    split List version and Array version

--- SelectionSort/List.lean
+++ SelectionSort/List.lean
@@ -1,25 +1,4 @@
-/-
-# 選択ソート
-選択ソートは，次のような手順で行われるソートアルゴリズムです．
-
--/
 import Mathlib.Tactic
-
-namespace Array
-
-variable {α : Type} [LinearOrder α]
-
-/-- 配列 `arr` が与えられたとき, `arr[i]` から `arr` の最後尾までの部分のうち
-最小の要素 `arr[min]` を探し，それを `arr[i]` と交換する -/
-def selectionSortLoop (arr : Array α) (i : Fin arr.size) : Array α :=
-  if i ≥ arr.size then
-    arr
-  else
-    sorry
-
-end Array
-
-namespace List
 
 variable {α : Type} [LinearOrder α]
 
@@ -59,5 +38,3 @@ def selectionSortLoop (l : List α) (i : Nat) : List α :=
 
         sorry
       sorry
-
-end List
