870cde1a2a427e7f9cff48fb2276d3983dc386bf
Seasawher <tellagoodstory@icloud.com>
2024-06-11 20:54:54 +0900

    update Lean

--- SelectionSort/Playground/Array.lean
+++
@@ -1,9 +0,0 @@
-import Mathlib.Tactic
-
-variable {α : Type} [LinearOrder α]
-
-def selectionSortLoop (arr : Array α) (i : Fin arr.size) : Array α :=
-  if i ≥ arr.size then
-    arr
-  else
-    sorry
