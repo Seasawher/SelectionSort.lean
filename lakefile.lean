2c83a01b2961a4b6066cf0af8d64c3b48e786f9f
Seasawher <tellagoodstory@icloud.com>
2024-10-05 04:27:13 +0900

    add linter

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,8 @@ package SelectionSort where
   -- add package configuration options here
   leanOptions := #[
     ⟨`autoImplicit, false⟩,
-    ⟨`relaxedAutoImplicit, false⟩
+    ⟨`relaxedAutoImplicit, false⟩,
+    ⟨`linter.flexible, true⟩
   ]
 
 require mathlib from git
